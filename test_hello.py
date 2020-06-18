4bda464a8ca39fe5986074f45435befd3b7e707e
pythianlopez <lopez@pythian.com>
2020-06-18 13:55:26 -0600

    deleted:    Dockerfile 	deleted:    myWebApp.py 	deleted:    requirements.txt 	deleted:    test_hello.py 	myApp/

--- test_hello.py
+++
@@ -1,8 +0,0 @@
-# test_hello.py
-from myWebApp import app
-
-def test_hello():
-    response = app.test_client().get('/')
-
-    assert response.status_code == 200
-    assert response.data == b'Ola k aze'
